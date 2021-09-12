b9287b8dfbd8a221ff5de105d855cd5e4e23f1d2
Raghu <me@example.com>
2021-09-12 19:30:25 +0530

    Processing code

---
+++ reports.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+from multiprocessing import Pool
+
+dr = "/data/feedback/"
+tasks = [i for i in os.walk(dr)][0][2]
+
+def run(task):
+    if not task.endswith(".txt"): return None
+    try:
+        file = dr + task
+        with open(file) as f:
+            text = [i.strip() for i in f.readlines()]
+        data = {"title": text[0], "name": text[1], "date": text[2], "feedback": text[3]}
+        req = requests.post("http://{}/feedback/", json=data)
+        req.raise_for_status()
+
+    except Exception as e:
+        print(task, e)
+
+p = Pool(len(tasks))
+p.map(run, tasks)
+
